f05361ad5d9ed0b77fd19410c683703cf05619f6
Patrick Glynn <pjglynn57@gmail.com>
2017-11-09 14:57:35 -0700

    added surge folder

---
+++ surge/readme.md
@@ -0,0 +1,15 @@
+This is a web application of the popular card game Blackjack that I created for my first-quarter project as part of Galvanize's full-stack web-development program.
+
+For the unfamiliar, blackjack pits players against the "house" to see who can get closest to 21 points without going over. Numbered cards (2-10) are worth the same value, face cards (J, Q, K) are worth 10, and aces can be worth either 1 or 11 depending on what is the most beneficial. At the outset, each player is dealt two cards. The player plays first, and can choose to "hit", and draw another card, or "stay", and end their turn. The player can draw as many cards as they like, but loses if they exceed 21 points. If the player ends their turn without going over 21, or "busting", the dealer plays, and will draw cards until they either tie or exceed the players value, or bust.
+
+I used HTML, CSS, JavaScript, and jQuery in this project, as well as the DeckOfCardsAPI (http://deckofcardsapi.com/).
+
+To ensure the function tests included in the repo work properly, you will need to install mocha and chai, and run ```npm init``` inside the cloned repo on your local machine.
+
+The deployed application can be found at http://p-glynn-q1-mvp.surge.sh/.
+
+There are also a few screenshots included for your viewing pleasure.
+
+
+-dependencies
+-surge
---
+++ surge/script.js
@@ -0,0 +1,401 @@
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,         function(m,key,value) {
+      vars[key] = value;
+    });
+  return vars;
+}
+var ct = 1;
+var userName = getUrlVars()['name'] || 'Player';
+var avatarID = getUrlVars()['avatar'] || 3;
+var reset = getUrlVars()['reset'];
+
+console.log(reset);
+
+
+
+var replacePlus = function (str) {
+  return str.replace(/\+/g, ' ');
+}
+
+userName = replacePlus(userName);
+
+
+
+var heroProx = 'https://g-blackjackapi.herokuapp.com/api/deck/';
+var heroDex = 'https://g-blackjackapi.herokuapp.com/api/deck/new/shuffle/?deck_count=6';
+var myID = 'i17qib8s5tgt';
+
+
+
+// var xhr = $.getJSON(heroDex);
+var xhr = $.getJSON(heroProx+myID+'/shuffle/');
+
+
+var deckID, card, cardValue, hiddenID, dealerDisplay, playerCt, dealerCt;
+var potAmt = 0;
+var betAmt = 0;
+
+// let userName = JSON.parse(localStorage.getItem('userName')) || {name: }
+let dealerWins = JSON.parse(localStorage.getItem('dealerWins')) || { wins: 0 };
+let userWins = JSON.parse(localStorage.getItem('userWins')) || { wins: 0 };
+
+let playerChips = JSON.parse(localStorage.getItem('playerChips')) || { chips: 1000};
+
+
+
+
+
+var playerArr = [];
+var dealerArr = [];
+
+
+$('document').ready(function () {
+  let chipCt = parseInt(JSON.stringify(playerChips.chips));
+  console.log("chips >>>", chipCt);
+
+
+  $('#player-name').text(userName);
+  $('.player-info').prepend($('<img>',{src:'img/avatars/'+avatarID+'.png', class:'avatar'}))
+
+  var updateText = function () {
+    $('.chip-count').text(`Chips: ${chipCt}`);
+    $('#bet-val').text(betAmt);
+  }
+
+  // function checkReset (reset) {
+  //   if (reset === 'yes') {
+  //     localStorage.setItem('playerChips', JSON.stringify(1000));
+  //     localStorage.setItem('dealerWins', JSON.stringify(0));
+  //     localStorage.setItem('userWins', JSON.stringify(0));
+  //   }
+  // }
+  // checkReset(reset);
+
+  updateText();
+
+  var aceReplace = function (arr) {
+    var out = [];
+    var aceCt = 0;
+    for (let card of arr) {
+        if (card === 'ACE') {
+          aceCt ++;
+        } else {
+          out.push(card);
+        }
+      }
+    for (let i=0; i<aceCt; i++) {out.push('ACE')}
+    return out;
+  }
+
+  var getValue = function (arr) {
+    arr = aceReplace(arr);
+    var arrVal = 0;
+    for (let card of arr) {
+      if (card === 'ACE')  {
+        if (arrVal + 11 > 21) {
+          arrVal += 1;
+        } else {
+          arrVal += 11;
+        }
+      } else if (card === 'KING' || card === 'QUEEN' || card === 'JACK') {
+        arrVal += 10;
+      } else {
+        arrVal += parseInt(card);
+      }
+    }
+    return arrVal;
+  }
+
+  var updatePlayerCount = function () {
+    playerCt = getValue(playerArr);
+    $('.player-counter').text(`Count: ${playerCt}`);
+  }
+
+  var updateDealerCount = function () {
+    dealerCt = getValue(dealerArr);
+    $('.dealer-counter').text(`Count: ${dealerCt}`);
+  }
+
+  updateText();
+
+  function checkReset (str) {
+    if (str.toLowerCase() === 'reset') {
+      localStorage.removeItem('dealerWins');
+      localStorage.removeItem('userWins');
+    }
+  }
+
+
+  // checkReset(resetLocal);
+
+
+  xhr.done(function (data) {
+
+    deckID = data.deck_id;
+    console.log(data);
+  });
+
+  var deal = function () {
+
+    betAmt += 10;
+    chipCt -= 10;
+    $('.chip-container').append($('<img>', {src:'img/chip.png', class:'chip-img', id:`10`}))
+    updateText();
+
+    $('#bet-more').prop("hidden", false);
+    $('#bet-less').prop("hidden", false);
+    $('#confirm-bet').prop("hidden", false);
+    $('#deal').prop("hidden", true);
+    $('.pot-tracker').prop("hidden", false);
+    $('.bet-tracker').prop("hidden", false);
+    $('#check-score').prop("hidden", true);
+    // $('#right-block').css('display', 'block')
+    $('.def-img').empty();
+    $('.dealer-card-container').append($('<img>',{src:'img/back1.jpg', class:'card-img', id:'placeholder'}));
+
+    var drawFour = heroProx+myID+'/draw/?count=4';
+    var drawXHR = $.getJSON(drawFour);
+    drawXHR.done(function (drawFourData) {
+
+      $('.dealer-card-container').append($('<img>',{src:'http://deckofcardsapi.com/static/img/'+drawFourData.cards[3].code+'.png', class:'card-img'}));
+      hiddenID = drawFourData.cards[1].code;
+
+      for (let i=0; i<4; i++) {
+        var card = drawFourData.cards[i].value;
+
+        if (i % 2 === 0) {
+          $('.player-card-container').append($('<img>',{id:`img${i}`,src:'http://deckofcardsapi.com/static/img/'+drawFourData.cards[i].code+'.png', class:'card-img'}));
+          playerArr.push(card);
+        } else {
+          dealerArr.push(card);
+        }
+      }
+      // playerCt = getValue(playerArr);
+      // $('.player-counter').text(playerCt);
+      updatePlayerCount();
+      dealerCt = getValue(dealerArr.slice(1));
+      $('.dealer-counter').text(`Count: ${dealerCt}`);
+
+    });
+    updateText();
+  };
+
+
+
+
+
+  // var draw = function () {
+  //   var drawOne = heroProx+myID+'/draw/?count=1';
+  //   var drawXHR = $.getJSON(drawOne);
+  //   drawXHR.done(function (drawOneData) {
+  //     card = drawOneData.cards[0].value;
+  //   })
+  //   return card;
+  // }
+  function getRand () {
+    return Math.floor(Math.random() * 5 + 1);
+  }
+
+  function hitAud() {
+    var rI = getRand();
+    var hit = $(`#hit-${rI}`);
+    hit[0].play();
+  }
+
+  function stayAud() {
+    var rI = getRand();
+    var stay = $(`#stay-${rI}`);
+    stay[0].play();
+  }
+
+
+  var hit = function(event) {
+    hitAud();
+    var drawOne = heroProx+myID+'/draw/?count=1';
+    var drawXHR = $.getJSON(drawOne);
+    drawXHR.done(function (drawOneData) {
+      card = drawOneData.cards[0].value;
+      playerArr.push(card);
+      // playerCt = getValue(playerArr);
+      //
+      // $('.player-counter').text(playerCt);
+      updatePlayerCount();
+      $('.player-card-container').append($('<img>',{src:'http://deckofcardsapi.com/static/img/'+drawOneData.cards[0].code+'.png', class:'card-img'}))
+      setTimeout(checkPlayer(), 2000);
+    });
+  };
+
+  var dealerHit = function(event) {
+
+    var drawOne = heroProx+myID+'/draw/?count=1';
+    var drawXHR = $.getJSON(drawOne);
+    drawXHR.done(function (drawOneData) {
+      card = drawOneData.cards[0].value;
+      dealerArr.push(card);
+      // $('.dealer-counter').text(getValue(dealerArr));
+      updateDealerCount();
+      $('.dealer-card-container').append($('<img>',{src:'http://deckofcardsapi.com/static/img/'+drawOneData.cards[0].code+'.png', class:'card-img'}))
+      setTimeout(checkDealer(), 2000);
+    });
+  };
+
+
+
+
+
+  var checkDealer = function () {
+    dealerCt = getValue(dealerArr);
+    playerCt = getValue(playerArr);
+    setTimeout(function () {
+      if (dealerCt < playerCt) {
+      dealerHit();
+      } else if (dealerCt > 21) {
+        // $('#dealer-bust').css('display', 'block');
+        $('.dealer-counter').append(' - BUST');
+        end();
+      } else {
+        end();
+      }
+    }, 1250);
+  }
+
+  var checkPlayer = function () {
+    setTimeout(function () {
+      if (playerCt > 21) {
+        // $('#player-bust').css('display', 'block');
+        $('.player-counter').append(' - BUST');
+        end();
+        // figure out a way to overlay the bust img over top of the cards?
+      }
+    }, 500);
+  }
+
+  var end = function () {
+    $('#stay').prop('hidden', true);
+    $('#hit').prop('hidden', true);
+    if (playerCt > 21) {
+      swal(`${userName} busted!`, "Dealer wins.", 'error');
+      updateText();
+      dealerWins.wins ++;
+      localStorage.setItem('playerChips', JSON.stringify(chipCt));
+      localStorage.setItem('dealerWins', JSON.stringify(dealerWins));
+    } else if (dealerCt > 21) {
+      swal(`${userName} wins!`, "Dealer busted.", "success");
+      chipCt += betAmt*2;
+      updateText();
+      localStorage.setItem('playerChips', JSON.stringify(chipCt));
+      userWins.wins ++;
+      localStorage.setItem('userWins', JSON.stringify(userWins))
+
+    } else if (playerCt === dealerCt) {
+      chipCt += betAmt;
+      swal({
+        text: `It's a tie! (${dealerCt})`,
+        type: 'warning',
+      });
+    } else {
+      updateText();
+      swal({
+        text:`Dealer (${dealerCt}) beats ${userName} (${playerCt}).`,
+        type:'error',
+      });
+      dealerWins.wins ++;
+      localStorage.setItem('playerChips', JSON.stringify(chipCt));
+      localStorage.setItem('dealerWins', JSON.stringify(dealerWins));
+    }
+
+    setTimeout (function () {
+      betAmt = 0;
+      ct = 1;
+      $('.player-card-container').empty();
+      $('.dealer-card-container').empty();
+      $('.def-img').append($("<img src='img/back1.jpg' class='default-img'/>"));
+      dealerArr = [];
+      updateDealerCount();
+      playerArr = [];
+      updatePlayerCount();
+      $('#dealer-playing').prop("hidden", true);
+      $('.chip-container').empty()
+      $('#deal').prop("hidden", false);
+      $('#check-score').prop("hidden", false);
+      $('.bet-tracker').prop("hidden", true);
+      updateText();
+  }, 1500);
+  }
+
+  var stay = function () {
+    stayAud();
+    $('#placeholder').remove();
+    $('#hit').prop("hidden", true);
+    $('#stay').prop("hidden", true);
+    $('#dealer-playing').prop("hidden", false);
+    $('.dealer-card-container').prepend($('<img>',{src:'http://deckofcardsapi.com/static/img/'+hiddenID+'.png', class:'card-img'}));
+    $('.dealer-counter').text(getValue(dealerArr));
+    setTimeout(checkDealer(), 1000);
+  }
+
+
+  var checkScore = function (event) {
+    swal(`${userName}: ${userWins.wins} wins.<br>Dealer: ${dealerWins.wins} wins.`)
+  }
+
+
+
+  var betMore = function (event) {
+    if (betAmt < 91) {
+      ct ++;
+      console.log(ct);
+      chipCt -= 10;
+      betAmt += 10;
+      $('.chip-container').append($('<img>', {src:'img/chip.png', class:'chip-img', id:`${ct}`}));
+    }
+
+    updateText();
+  }
+
+  var betLess = function (event) {
+    if (betAmt > 10) {
+      $(`#${ct}`).remove();
+      ct--;
+      chipCt += 10;
+      betAmt -= 10;
+
+    }
+
+    updateText();
+  }
+
+  var confirmBet = function (event) {
+    $('#bet-more').prop("hidden", true);
+    $('#bet-less').prop("hidden", true);
+    $('#confirm-bet').prop("hidden", true);
+    $('#hit').prop("hidden", false);
+    $('#stay').prop("hidden", false);
+  }
+
+
+
+  $('#hit').click(hit);
+  $('#stay').click(stay);
+  $('#deal').click(deal);
+  $('#check-score').click(checkScore);
+  $('#bet-more').click(betMore);
+  $('#bet-less').click(betLess);
+  $('#confirm-bet').click(confirmBet);
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+// var deckAPI = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6';
